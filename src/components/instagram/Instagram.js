9f3757c2c92e3bfcf493a3444e7e94b63ae6e4ca
shuyin21 <shuyindev@gmail.com>
2022-07-13 21:36:39 +0100

    instagram images changed to 6

--- src/components/instagram/Instagram.js
+++ src/components/instagram/Instagram.js
@@ -8,7 +8,7 @@ const Instagram = () => {
         <>
             <InstaDiv >
                 <InstaWrapper>
-                    <InstagramFeed token={"IGQVJValk2VFRJS2V1a2RnRmQ3elloN2tfVEltcGwyTE9ySWdvVnVBel9SUTFKMlRPV1dtcHJ6MkdvSGVndXpaUFJSbUpVWTkxYUxydEttU1VFRFF5VGxfVkFIWjR0aERrd1RNUTdJekZAJMWl4SWlnRgZDZD"} counter="3" />
+                    <InstagramFeed token={"IGQVJValk2VFRJS2V1a2RnRmQ3elloN2tfVEltcGwyTE9ySWdvVnVBel9SUTFKMlRPV1dtcHJ6MkdvSGVndXpaUFJSbUpVWTkxYUxydEttU1VFRFF5VGxfVkFIWjR0aERrd1RNUTdJekZAJMWl4SWlnRgZDZD"} counter="6" />
                 </InstaWrapper>
             </InstaDiv>
         </>
